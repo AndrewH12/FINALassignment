b349414f3c732e61df17c9dfeaa08bf7b0563a3a
342393436 <342393436@C-G5JWJB2.tdsb.on.ca>
2025-06-05 11:36:44 -0400

    start page and bar

--- src/finalassignment/MySketch.java
+++ src/finalassignment/MySketch.java
@@ -11,6 +11,9 @@ import processing.core.PApplet;
 public class MySketch extends PApplet{
   private Person person;
   private Person person1;
+  public String health;
+  String userInput = "";
+  int stage = 0;
  
   
   public void settings() {
@@ -19,33 +22,52 @@ public class MySketch extends PApplet{
 
   public void setup() {
     background(255);
-    person = new Person(this, 200, 200, "Amdrew",20 ,"images/person.png");
-    person1 = new Person(this, 150, 150, "Amdrew",20 ,"images/person.png");
+    textSize(20);
+    person = new Person(this, 200, 200, "Andrew","100",20 ,"images/person.png");
+    person1 = new Person(this, 150, 150, "Andrew","100",20 ,"images/person.png");
   }
   
   public void draw() {
     background(255);
-    person.draw();
-         
-    if (keyPressed) {
-        if (keyCode == LEFT) {
-            person1.move(-2, 0);
-        } else if (keyCode == RIGHT) {
-            person1.move(2, 0);
-        } else if (keyCode == UP) {
-            person1.move(0, -2);
-        } else if (keyCode == DOWN) {
-            person1.move(0, 2);
-         }
+    if (stage ==0){
+        fill(0);
+        text("Game START:", 140,150);
+        text("PRESS ENTER TO START...", 100,200);
+        text(userInput, 200,300);
+    }else if(stage ==1){
+        person1.displayInfo(this);
+        person.draw();
+        person1.draw();    
+        if (keyPressed) {
+            if (keyCode == LEFT) {
+                person1.move(-2, 0);
+            } else if (keyCode == RIGHT) {
+                person1.move(2, 0);
+            } else if (keyCode == UP) {
+                person1.move(0, -2);
+            } else if (keyCode == DOWN) {
+                person1.move(0, 2);
+             }
+        }
+
+        if (person.isCollidingWith(person1)){
+            fill(255,0,0);
+            this.text("oouch",person1.x, person1.y);
+            String health ="0";
+        }
     }
-    person1.draw();
     
-    if (person.isCollidingWith(person1)){
-        fill(255,0,0);
-        this.text("oouch",person1.x, person1.y);
     }
-    
-    }
+    public void keyPressed(){
+        if (stage==0){
+            if (keyCode ==ENTER){
+                stage = 1;
+            }else if(key != CODED){
+                userInput +=key;
+            }
+            
+        }
+    } 
   }
 
 
--- src/finalassignment/Person.java
+++ src/finalassignment/Person.java
@@ -15,13 +15,15 @@ public class Person {
     public int x, y;
     private String name;
     private int age;
+    public String health;
     private PApplet app;
     private PImage image; 
-    public Person(PApplet p, int x, int y, String name, int age,String imagePath ){
+    public Person(PApplet p, int x, int y, String name,String health, int age,String imagePath ){
         this.app = p;
         this.x =x;
         this.y=y;
         this.name = name;
+        this.health = health;
         this.age = age;
         this.image = app.loadImage(imagePath);
     }
@@ -42,5 +44,10 @@ public class Person {
         float d = PApplet.dist(otherCenterX, otherCenterY, centerX, centerY);
         
         return d <32;
+    }
+    public void displayInfo(PApplet p){
+        app.fill(0);
+        app.text("Name:"+ name, x,y-50);
+        app.text("Health:"+ health, x,y-30);
     }    
 }
